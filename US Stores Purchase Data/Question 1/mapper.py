fcd01a6d985f025acc5dac347080ff7f102a6464
nsk160130 <niks.ce15@gmail.com>
2017-02-20 05:55:12 -0600

    Update mapper.py

--- US Stores Purchase Data/Question 1/mapper.py
+++ US Stores Purchase Data/Question 1/mapper.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python
 
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# This time we want two different elemetns: Item category and the Cost
+# We need to write them out to standard output, separated by a tab, so the reducer can read it from standard input.
+
+
+import sys
+
+for line in sys.stdin:
+	data = line.strip().split("\t")
+	
+	if len(data) != 6:
+		continue
+
+	date,time,store,item,cost,payment = data
+	print "{0}\t{1}".format(item,cost)   ## Print out the Item category and the cost.
